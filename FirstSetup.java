bb2b814581698c273cf5a0cac46b994a83adf1f1
MINAFOUADFAWZY <113135987+MINAFOUADFAWZY@users.noreply.github.com>
2022-09-21 20:40:30 +0200

    Update FirstSetup.java

--- FirstSetup.java
+++ FirstSetup.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gstbilling;
 
 import java.io.File;
@@ -20,10 +16,6 @@ import javax.swing.JOptionPane;
 
 
 
-/**
- *
- * @author Rishabh
- */
 public class FirstSetup extends javax.swing.JFrame {
 
     /**
