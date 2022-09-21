4a7c97e5c55b26fb1494cbd2a8e7665eb7c2f7d3
MINAFOUADFAWZY <113135987+MINAFOUADFAWZY@users.noreply.github.com>
2022-09-21 20:43:33 +0200

    Update DateFill.java

--- admin/DateFill.java
+++ admin/DateFill.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package gstbilling.admin;
 
 import java.util.Date;
 
-/**
- *
- * @author Rishabh
- */
+
 public class DateFill {
     private static String date1,month,year;
 
