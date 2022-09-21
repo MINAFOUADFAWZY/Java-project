b9994fbcccead1831b3e706edb137164d6e822e6
MINAFOUADFAWZY <113135987+MINAFOUADFAWZY@users.noreply.github.com>
2022-09-21 20:50:57 +0200

    Update PreviousInvoice.java

--- employee/PreviousInvoice.java
+++ employee/PreviousInvoice.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gstbilling.employee;
 
 import gstbilling.Connect;
@@ -12,10 +8,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import net.proteanit.sql.DbUtils;
 
-/**
- *
- * @author Rishabh
- */
 public class PreviousInvoice extends javax.swing.JInternalFrame {
 
     /**
