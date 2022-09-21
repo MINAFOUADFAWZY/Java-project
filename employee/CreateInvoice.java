0c14d07d2bba65dac62e99ceb20aed7ef185cb3b
MINAFOUADFAWZY <113135987+MINAFOUADFAWZY@users.noreply.github.com>
2022-09-21 20:48:56 +0200

    Update CreateInvoice.java

--- employee/CreateInvoice.java
+++ employee/CreateInvoice.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gstbilling.employee;
 
 import gstbilling.Connect;
@@ -24,10 +20,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Rishabh
- */
+
 public class CreateInvoice extends javax.swing.JInternalFrame {
 
     /**
