3a23c66b4e93b56cc310e2987e21bf71c0890fca
MINAFOUADFAWZY <113135987+MINAFOUADFAWZY@users.noreply.github.com>
2022-09-21 20:44:57 +0200

    Update GstClassificationCreation.java

--- admin/GstClassificationCreation.java
+++ admin/GstClassificationCreation.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gstbilling.admin;
 
 
@@ -12,10 +8,7 @@ import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
 
 
-/**
- *
- * @author Rishabh
- */
+
 public class GstClassificationCreation extends javax.swing.JInternalFrame {
 
     /**
