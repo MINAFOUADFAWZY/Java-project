3cc33a84426374d81e7158d990983bb1cc752fbc
MINAFOUADFAWZY <113135987+MINAFOUADFAWZY@users.noreply.github.com>
2022-09-21 20:52:50 +0200

    Update ViewProduct.java

--- employee/ViewProduct.java
+++ employee/ViewProduct.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gstbilling.employee;
 
 import gstbilling.Connect;
@@ -10,10 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import net.proteanit.sql.DbUtils;
-/**
- *
- * @author Rishabh
- */
+
 public class ViewProduct extends javax.swing.JInternalFrame {
 
     /**
