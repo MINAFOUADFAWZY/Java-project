2004998913196105931716abebc982a356d921c8
MINAFOUADFAWZY <113135987+MINAFOUADFAWZY@users.noreply.github.com>
2022-09-21 20:51:44 +0200

    Update UpdateInvoice.java

--- employee/UpdateInvoice.java
+++ employee/UpdateInvoice.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gstbilling.employee;
 
 import gstbilling.Connect;
@@ -26,12 +22,7 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author Shivam Kumar
- */
 public class UpdateInvoice extends javax.swing.JInternalFrame {
     private Object jinvoice;
    public static String name;
