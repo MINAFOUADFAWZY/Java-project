8461f4863e24f2c4ba901917e6d0f2a1f1fb9346
MINAFOUADFAWZY <113135987+MINAFOUADFAWZY@users.noreply.github.com>
2022-09-21 20:52:14 +0200

    Update User.java

--- employee/User.java
+++ employee/User.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gstbilling.employee;
 
-/**
- *
- * @author Shivam Kumar
- */
+
 public class User {
     int sno;
     private double cgst,sgst,igst,price,qty,amount,discount;
